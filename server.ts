8cb19d08800cb1092974fdd95cf43ee2ed68d85d
Kamran Elchuzade <elchuzade@gmail.com>
2019-06-07 15:10:31 +0300

    added restriction to duplicate github user names

--- server.ts
+++ server.ts
@@ -20,7 +20,7 @@ const options: DBOptions = {
   user: 'kamranelchuzade',
   password: 'postgres',
   host: 'localhost',
-  database: 'lovelystay_test'
+  database: 'mydb'
 };
 
 console.info(
@@ -30,7 +30,7 @@ console.info(
 
 const pgpDefaultConfig = {
   promiseLib: require('bluebird'),
-  // Log all querys
+  // Log all queries
   query(query) {
     console.log('[SQL   ]', R.take(trimLogsSize, query.query));
   },
@@ -44,6 +44,11 @@ const pgpDefaultConfig = {
 
 interface GithubUsers {
   id: number;
+  location: string;
+  name: string;
+  company: string;
+  login: string;
+  public_repos: number;
 }
 
 const pgp = pgPromise(pgpDefaultConfig);
@@ -58,24 +63,32 @@ rl.question(
   'Hello there! Here we will insert a GitHub user to the database through API, please type a username and press Enter (Return on some keyboards)',
   username => {
     db.none(
-      'CREATE TABLE IF NOT EXISTS github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT)'
-    )
-      .then(() =>
-        request({
-          uri: `https://api.github.com/users/${username}`,
-          headers: {
-            'User-Agent': 'Request-Promise'
-          },
-          json: true
-        })
-      )
-      .then((data: GithubUsers) => 
-        db.one(
-          'INSERT INTO github_users (login) VALUES ($[login]) RETURNING id',
-          data
-        )
-      )
-      .then(({ id }) => console.log(id));
+      'CREATE TABLE IF NOT EXISTS github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT, public_repos INTEGER, location TEXT)'
+    ).then(() => {
+      db.query('SELECT * FROM github_users where login = $1', [username]).then(
+        result => {
+          if (result.length > 0) {
+            console.log('this name already exists');
+            process.exit(0);
+          } else {
+            return request({
+              uri: `https://api.github.com/users/${username}`,
+              headers: {
+                'User-Agent': 'Request-Promise'
+              },
+              json: true
+            })
+              .then((data: GithubUsers) =>
+                db.one(
+                  'INSERT INTO github_users (login, name, company, public_repos, location) VALUES ($[login], $[name], $[company], $[public_repos], $[location]) RETURNING id',
+                  data
+                )
+              )
+              .then(({ id }) => console.log(id));
+          }
+        }
+      );
+    });
     rl.close();
   }
 );
