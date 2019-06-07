7e7681eb5f488a11049ecc22af4b73a35da60642
Kamran Elchuzade <elchuzade@gmail.com>
2019-06-07 22:16:56 +0300

    added menu with choices 1 and 2 and typing a username to add to db also made it repeat until the app is closed

--- server.ts
+++ server.ts
@@ -58,37 +58,81 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+const initialMessage =
+  'Please type a github username to add to our db or use menu to read our db: 1 - lisbon, 2 - stats : ';
+rl.question(`Hello there! ${initialMessage}`, answer => {
+  let msg = answerCheck(answer);
+  userReplay(msg);
+});
+rl.on('close', () => {
+  console.log('Have a nice day! :)');
+});
 
-rl.question(
-  'Hello there! Here we will insert a GitHub user to the database through API, please type a username and press Enter (Return on some keyboards)',
-  username => {
+const answerCheck = answer => {
+  let msg = '';
+  if (answer.trim().length > 39) {
+    // Github does not accept usernames longer than 39 characters
+    console.log('username must be between 1 and 39 characters');
+  } else if (answer.length === 0) {
+    console.log(
+      'empty username definitely does not exist on github, try again'
+    );
+  } else if (answer === '1') {
+    console.log('lisbon stuff');
+    msg = 'you are checking a lisbon stuff ';
+  } else if (answer === '2') {
+    console.log('table stuff');
+    msg = 'you are checking a table stuff ';
+  } else {
+    console.log('add a user to db');
+
     db.none(
       'CREATE TABLE IF NOT EXISTS github_users (id BIGSERIAL, login TEXT, name TEXT, company TEXT, public_repos INTEGER, location TEXT)'
     ).then(() => {
-      db.query('SELECT * FROM github_users where login = $1', [username]).then(
-        result => {
-          if (result.length > 0) {
-            console.log('this name already exists');
-            process.exit(0);
-          } else {
-            return request({
-              uri: `https://api.github.com/users/${username}`,
-              headers: {
-                'User-Agent': 'Request-Promise'
-              },
-              json: true
-            })
-              .then((data: GithubUsers) =>
-                db.one(
-                  'INSERT INTO github_users (login, name, company, public_repos, location) VALUES ($[login], $[name], $[company], $[public_repos], $[location]) RETURNING id',
-                  data
-                )
+      db.query('SELECT * FROM github_users where login = $1', [
+        answer.trim()
+      ]).then(result => {
+        if (result.length > 0) {
+          console.log('this name already exists');
+          // process.exit(0);
+          msg = 'This github user name already exists, try another one! ';
+          console.log(
+            `This github user name already exists, try another one! ${initialMessage}`
+          );
+          return;
+        } else {
+          return request({
+            uri: `https://api.github.com/users/${answer}`,
+            headers: {
+              'User-Agent': 'Request-Promise'
+            },
+            json: true
+          })
+            .then((data: GithubUsers) =>
+              db.one(
+                'INSERT INTO github_users (login, name, company, public_repos, location) VALUES ($[login], $[name], $[company], $[public_repos], $[location]) RETURNING id',
+                data
               )
-              .then(({ id }) => console.log(id));
-          }
+            )
+            .then(({ id }) => {
+              msg = `Great, you have added a new user number ${id} and you can add more users! `;
+              console.log(
+                `Great, you have added a new user number ${id} and you can add more users! ${initialMessage}`
+              );
+            });
         }
-      );
+      });
     });
-    rl.close();
   }
-);
+  return msg;
+};
+
+const userReplay = msg => {
+  rl.setPrompt(`${msg} ${initialMessage}`);
+  rl.prompt();
+  rl.on('line', answer => {
+    let msg = answerCheck(answer);
+    rl.setPrompt(`${msg}${initialMessage}`);
+    rl.prompt();
+  });
+};
